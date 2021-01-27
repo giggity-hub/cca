d813cc6d2a32c089bed4d969a3f56368cb7386f2
giggity-hub <imigstefan@gmail.com>
2021-01-27 15:53:44 +0100

    test Webpage aufgesetzt. Kann Post request entgegen nehmen

--- VR/src/servlets/Test.java
+++ VR/src/servlets/Test.java
@@ -8,11 +8,14 @@ package servlets;
 //ich bin der uwe und ich bin auch dabei
 //Alex ist wieder da
 import java.io.IOException;
+import java.sql.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import application.CCApplication;
 
 /**
  * 
@@ -45,12 +48,42 @@ public class Test extends HttpServlet {
 			e.printStackTrace();
 		}
 	}
-
+	protected int[] participantArrayFromString(String participants) {
+		String[] strArr = participants.split(",");
+		int[] intArr = new int[strArr.length];
+		for (int i=0; i< strArr.length; i++) {
+			intArr[i]= Integer.parseInt(strArr[i]);
+		}
+		return intArr;
+	}
 	/**
 	 * Call doGet instead of doPost
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) {
+		if (request.getParameter("action").equals("addAppointment")) {
+			System.out.println("Registered Post request with action=addAppointment");
+			
+			int creator = 69;
+			int groupId = 420;
+			Date[] dates = {Date.valueOf((String) request.getParameter("dates"))}; //only one date for now
+			int[] participants = participantArrayFromString(request.getParameter("participants"));
+			String description = (String)request.getParameter("description");
+			String name = (String)request.getParameter("name");
+			String location = (String) request.getParameter("location");
+			int duration = Integer.parseInt(request.getParameter("duration"));
+			Date deadline = Date.valueOf((String) request.getParameter("dates"));
+			
+			
+			System.out.println(dates);
+			System.out.println(duration);
+			System.out.println(participants[1]);
+			
+			//call the CCA singleton to post the appontment
+//			CCApplication.getInstance().postAppointment(creator, dates, participants, description, name, location, duration, deadline, groupId);
+
+		}
+		
 		doGet(request, response);
 	}
 }
